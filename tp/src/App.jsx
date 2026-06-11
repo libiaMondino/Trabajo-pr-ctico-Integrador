6a47bbe792e41b36884747ed75851a10ce1e2136
camilacarmona <camila-ccarmona@hotmail.com>
2026-06-11 03:43:30 -0300

    Navbar y inicio avanzados

--- tp/src/App.jsx
+++ tp/src/App.jsx
@@ -1,12 +1,19 @@
-//import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Carrito from "./components/Carrito/Carrito";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   //Rutas e importar componentes
   return (
     <>
-     <h1>Holis</h1>
-     <Carrito/>
+     <Navbar/>
+     
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carrito" element={<Carrito />} />
+      </Routes>
+     
     </>
   )
 }
