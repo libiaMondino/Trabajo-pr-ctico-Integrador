c1fcafe90f876f983a077ffb9d79c2dd5258e7dc
Libia Mondino <libiamondino02@gmail.com>
2026-06-18 08:53:56 -0300

    Avance backend

---
+++ tp/backend/routes/productos.routes.js
@@ -0,0 +1,19 @@
+/*LO QUE FALTA
+* Revisar las rutas de cada método(admin, superadmin,usuario)
+*/ 
+import { Router } from "express";
+import { encontrarProducto, encontrarProductos, crearProducto, actualizarProducto, eliminarProducto } from "../services/producto.services";
+const router = Router();
+
+/*Acciones que pueden realizar todos*/
+router.get("/productos/:categoria?", encontrarProductos);
+router.get("/productos/:categoria?/:id", encontrarProducto);
+
+/*Solo admin y Superadmin*/ 
+router.post("/nuevo-producto",crearProducto);
+
+router.put("/productos/:categoria?/:id", actualizarProducto);
+
+router.delete("/productos/:categoria?/:id", eliminarProducto);
+
+export default router;
